e6d1e803ea6c0ea63f6552ba69b25bd3b0f0b25d
PMapa <paulo.hp126@gmail.com>
2024-06-15 13:17:26 -0300

    aprendendo a usar angular

--- itamara_com/src/App.jsx
+++ itamara_com/src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css';
+import React from "react";
+import { Artigo } from "./components/mostrarCounter";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = { mostrarArtigo: false }
+
+  }
+  render() {
+    return (
+      <>
+        <h1>SysItamara</h1>
+
+        <button onClick={() => {
+          this.setState({ mostrarArtigo: !this.state.mostrarArtigo });
+        }}
+        >
+          <div>
+            {this.state.mostrarArtigo ? "Veja outras suítes" : "Suíte Supra"}
+          </div>
+        </button>
+
+        {this.state.mostrarArtigo && <Artigo />}
+        {/* {this.state.mostrarArtigo ? <Artigo /> : null} */}
+      </>
+    );
+  }
 }
-
 export default App;
---
+++ itamara_com/src/components/mostrarCounter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Artigo extends React.Component {
+    render() {
+        return  (
+            <div>
+                <h6>Quarto Supra whauhawu</h6>
+                <img src="../imgs/logo.PNG" alt="" />
+                
+            </div>
+        )
+    }
+}
+
